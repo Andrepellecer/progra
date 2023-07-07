b52e406c9e63006cf127f7dfb79d77c3d26d8144
Andrepellecer <128725044+Andrepellecer@users.noreply.github.com>
2023-07-07 08:24:48 -0600

    mascotaenemigo

--- Js/pokemon.js
+++ Js/pokemon.js
@@ -1,21 +1,87 @@
-function iniciarjuego(){
-    let buttonmascotajudador =document.getElementById("boton-pokemon")
-    buttonmascotajudador.addEventListener("click",seleccionarmascotajugador)
+function iniciarJuego() {
+
+  let botonMascotaJugador = document.getElementById('boton-mascota')
+
+  botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
 }
 
-function seleccionarmascotajugador(){
-    let inputcharmander = document.getElementById("charmander")
-    let inputbulbasaur= document.getElementById("bulbasaur")
-    let inputsquirtle = document.getElementById("squirtle")
-    if(inputcharmander.checked){
-     alert("Elejiste Charmander") 
-    } else if (inputbulbasaur.checked){
-     alert("Elejiste bulbasaur ")
-    } else if (inputsquirtle.checked){
-      alert("Elejiste squirtle")  
-    } else { alert("escoge un pokemon")
 
-    } 
+
+
+function seleccionarMascotaJugador() {
+
+  let inputCharmander = document.getElementById('Charmander')
+
+  let inputBulbasaur = document.getElementById('Bulbasaur')
+
+  let inputSquirte = document.getElementById('Squirte')
+
+  let spanMascotaEnimigo = document.getElementById('mascota-jugador')
+
+
+
+
+  if (inputCharmander.checked) {
+
+      spanMascotaEnimigo.innerHTML ='Charmander'
+
+  } else if (inputBulbasaur.checked) {
+
+      spanMascotaEnimigo.innerHTML = 'Bulbasaur'
+
+  } else if (inputSquirte.checked) {
+
+      spanMascotaEnimigo.innerHTML = 'Squirte'
+
+  } else {
+
+      alert('Selecciona una mascota')
+
+  }
+
+
+
+
+  seleccionarMascotaEnemigo()
+
 }
 
-window.addEventListener("load", iniciarjuego)
+
+
+
+function seleccionarMascotaEnemigo(){
+
+  let ataqueAletorio = aleatorio(1,3)
+
+  let spanMascotaEnimigo = document.getElementById('mascota-enemigo')
+
+
+
+
+  if (ataqueAletorio ==1){
+
+      spanMascotaEnimigo.innerHTML = 'Charmander'
+
+  } else if (ataqueAletorio ==2){
+
+      spanMascotaEnimigo.innerHTML = 'Bulbasaur'
+
+  } else {
+
+      spanMascotaEnimigo.innerHTML = 'Squirte'
+
+  }
+
+}
+
+
+
+
+function aleatorio(min, max){
+
+  return Math.floor(Math.random()* (max-min +1) +min)
+
+}
+
+window.addEventListener('load', iniciarJuego)
